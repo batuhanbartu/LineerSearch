4499d097437f3c439b5af79b7d4c2184bef202f3
batuhanbartu <batuhanmurat07@gmail.com>
2022-11-10 16:00:38 +0300

    Proje dosyası ekle.

---
+++ soru 16 ödev/soru 16 ödev.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+//ARDIŞIK ARAMA
+int main()
+{
+    int dizi[] = { 1,2,3,4,5 };
+    int arananacakSayi;
+    int i = 0;
+
+    cout << "Arama yapilacak dizi: ";
+    for (int k = 0; k < 5; k++) 
+    {
+        cout << " " << dizi[k];
+    }
+    cout << endl;
+    cout << "Aranacak sayi: ";
+    cin >> arananacakSayi;
+    int kontrol = 0;
+
+    while (i < 5) 
+    {
+        if (dizi[i] == arananacakSayi) 
+        {
+            kontrol = 1;
+            cout << arananacakSayi << " sayisi dizide vardir." << endl;
+            break;
+        }
+        i = i + 1;
+    }
+    if (kontrol == 0) 
+    {
+        cout << arananacakSayi << " sayısı dizide yoktur." << endl;
+    }
+    return 0;
+}
+
